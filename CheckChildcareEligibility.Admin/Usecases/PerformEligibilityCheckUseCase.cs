ea9be36c168d8a765efcae9694d215eb96fcabe3
Jenna Barton <jenna.barton@education.gov.uk>
2025-05-02 10:11:25 +0100

    Update the validation for parent details. Eligibility check use case changes as model is parent model now contains less parameters.

--- CheckChildcareEligibility.Admin/Usecases/PerformEligibilityCheckUseCase.cs
+++ CheckChildcareEligibility.Admin/Usecases/PerformEligibilityCheckUseCase.cs
@@ -27,7 +27,6 @@ public class PerformEligibilityCheckUseCase : IPerformEligibilityCheckUseCase
         ParentGuardian parentRequest,
         ISession session)
     {
-        session.Set("ParentFirstName", Encoding.UTF8.GetBytes(parentRequest.FirstName ?? string.Empty));
         session.Set("ParentLastName", Encoding.UTF8.GetBytes(parentRequest.LastName ?? string.Empty));
 
         // Build DOB string
@@ -38,20 +37,8 @@ public class PerformEligibilityCheckUseCase : IPerformEligibilityCheckUseCase
         ).ToString("yyyy-MM-dd");
 
         session.Set("ParentDOB", Encoding.UTF8.GetBytes(dobString));
-        session.SetString("ParentEmail", parentRequest.EmailAddress);
 
-        // If we're finishing a NASS flow, store "ParentNASS"; 
-        // otherwise store "ParentNINO".
-        if (parentRequest.NinAsrSelection == ParentGuardian.NinAsrSelect.AsrnSelected)
-        {
-            session.Set("ParentNASS", Encoding.UTF8.GetBytes(parentRequest.NationalAsylumSeekerServiceNumber ?? ""));
-            session.Remove("ParentNINO");
-        }
-        else
-        {
-            session.Set("ParentNINO", Encoding.UTF8.GetBytes(parentRequest.NationalInsuranceNumber ?? ""));
-            session.Remove("ParentNASS");
-        }
+        session.Set("ParentNINO", Encoding.UTF8.GetBytes(parentRequest.NationalInsuranceNumber ?? ""));
 
         // Build ECS request
         var checkEligibilityRequest = new CheckEligibilityRequest_Fsm
@@ -60,7 +47,6 @@ public class PerformEligibilityCheckUseCase : IPerformEligibilityCheckUseCase
             {
                 LastName = parentRequest.LastName,
                 NationalInsuranceNumber = parentRequest.NationalInsuranceNumber?.ToUpper(),
-                NationalAsylumSeekerServiceNumber = parentRequest.NationalAsylumSeekerServiceNumber?.ToUpper(),
                 DateOfBirth = dobString
             }
         };
