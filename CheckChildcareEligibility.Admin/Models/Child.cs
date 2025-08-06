bf20ccda3b70b8427191f49203f94b0e48838deb
Jenna Barton <jenna.barton@education.gov.uk>
2025-08-06 14:28:36 +0100

    Add Working Families (WF) option to Eligibility Types. Create Single Check Menu link for WF. New EnterDetails form for WF and the related Child model and ParentAndChildViewModel combining Parent and Child models.

---
+++ CheckChildcareEligibility.Admin/Models/Child.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using CheckChildcareEligibility.Admin.Attributes;
+
+namespace CheckChildcareEligibility.Admin.Models;
+
+public class Child
+{
+    //[NotMapped] public int ChildIndex { get; set; }
+
+    //[ChildName("first name")] public string? FirstName { get; set; }
+
+    //[ChildName("last name")]
+    //[LastName("last name", "child", "ChildIndex")]
+    //public string? LastName { get; set; }
+
+    public string? EligibilityCode { get; set; }
+
+    [NotMapped]
+    [Dob("date of birth", "child", "ChildIndex", "Day", "Month", "Year", true, true)]
+    public string? ChildDateOfBirth { get; set; }
+
+    public string? Day { get; set; }
+
+    public string? Month { get; set; }
+
+    public string? Year { get; set; }
+
+    //public School? School { get; set; }
+}
---
+++ CheckChildcareEligibility.Admin/ViewModels/ParentAndChildViewModel.cs
@@ -0,0 +1,23 @@
+﻿using CheckChildcareEligibility.Admin.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace CheckChildcareEligibility.Admin.ViewModels
+{
+    public class ParentAndChildViewModel
+    {
+        public string EligibilityType { get; set; } = string.Empty;
+        public string EligibilityTypeLabel { get; set; } = string.Empty;
+
+        [MaxLength(11)] public string EligibilityCode { get; set; } = string.Empty;
+
+        [Nino][MaxLength(13)] public string? NationalInsuranceNumber { get; set; }
+
+        public string? ChildDateOfBirth { get; set; }
+
+        public string? Day { get; set; }
+
+        public string? Month { get; set; }
+
+        public string? Year { get; set; }
+    }
+}
