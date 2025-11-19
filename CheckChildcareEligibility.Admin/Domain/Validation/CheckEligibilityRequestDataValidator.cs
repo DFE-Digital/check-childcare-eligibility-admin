cafb1197902fe0d3f69cb0adeda15229d383a8ae
lnedeleva <lili.nedeleva@education.gov.uk>
2025-11-19 13:28:56 +0000

    add cascade to validation rules so it stops on first error for each  rule

--- CheckChildcareEligibility.Admin/Domain/Validation/CheckEligibilityRequestDataValidator.cs
+++ CheckChildcareEligibility.Admin/Domain/Validation/CheckEligibilityRequestDataValidator.cs
@@ -14,16 +14,19 @@ public class CheckEligibilityRequestDataValidator : AbstractValidator<IEligibili
         When(x => x is CheckEligibilityRequestData, () =>
         {
             RuleFor(x => ((CheckEligibilityRequestData)x).LastName)
-           .NotEmpty().WithMessage(ValidationMessages.RequiredLastName)
-           .Must((x, lastName) => string.IsNullOrEmpty(lastName) || DataValidation.BeAValidName(lastName))
-           .WithMessage(ValidationMessages.ValidLastName);
+               .Cascade((CascadeMode.Stop))
+               .NotEmpty().WithMessage(ValidationMessages.RequiredLastName)
+               .Must((x, lastName) => string.IsNullOrEmpty(lastName) || DataValidation.BeAValidName(lastName))
+               .WithMessage(ValidationMessages.ValidLastName);
 
             RuleFor(x => ((CheckEligibilityRequestData)x).DateOfBirth)
+                .Cascade((CascadeMode.Stop))
                 .NotEmpty().WithMessage(ValidationMessages.RequiredDOB)
                 .Must((x, dob) => string.IsNullOrEmpty(dob) || DataValidation.BeAValidDate(dob))
                 .WithMessage(ValidationMessages.ValidDOB);
 
             RuleFor(x => ((CheckEligibilityRequestData)x).NationalInsuranceNumber)
+                .Cascade((CascadeMode.Stop))
                 .NotEmpty().WithMessage(ValidationMessages.RequiredNI)
                 .Must((x, NINumber) => string.IsNullOrEmpty(NINumber) || DataValidation.BeAValidNi(NINumber))
                 .WithMessage(ValidationMessages.ValidNI);
@@ -32,16 +35,19 @@ public class CheckEligibilityRequestDataValidator : AbstractValidator<IEligibili
         When(x => x is CheckEligibilityRequestWorkingFamiliesData, () =>
         {
             RuleFor(x => ((CheckEligibilityRequestWorkingFamiliesData)x).NationalInsuranceNumber)
+             .Cascade((CascadeMode.Stop))
              .NotEmpty().WithMessage(ValidationMessages.RequiredNI)
              .Must((x, NINumber) => string.IsNullOrEmpty(NINumber) || DataValidation.BeAValidNi(NINumber))
              .WithMessage(ValidationMessages.ValidNI);
 
             RuleFor(x => ((CheckEligibilityRequestWorkingFamiliesData)x).DateOfBirth)
+                .Cascade((CascadeMode.Stop))
                 .NotEmpty().WithMessage(ValidationMessages.RequiredDOB)
                 .Must((x, dob) => string.IsNullOrEmpty(dob) || DataValidation.BeAValidDate(dob))
                 .WithMessage(ValidationMessages.ValidDOB);
 
             RuleFor(x => ((CheckEligibilityRequestWorkingFamiliesData)x).EligibilityCode)
+                .Cascade((CascadeMode.Stop))
                 .NotEmpty().WithMessage(ValidationMessages.RequiredEligibilityCode)
                 .Must(x => long.TryParse(x, out _)).WithMessage(ValidationMessages.EligibilityCodeNumber)
                 .Must(x => x.Length == 11).WithMessage(ValidationMessages.EligibilityCodeIncorrectLength);
