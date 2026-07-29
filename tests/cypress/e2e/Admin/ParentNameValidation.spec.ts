77f79e201decd73c37571d2f0b3b901749de5c1a
Jenna Barton <jenna.barton@education.gov.uk>
2026-07-29 15:00:10 +0100

    Refactor Cypress test.

--- tests/cypress/e2e/Admin/ParentNameValidation.spec.ts
+++ tests/cypress/e2e/Admin/ParentNameValidation.spec.ts
@@ -1,12 +1,6 @@
 describe('Test that approved accented characters are accepted in name input fields', () => {
-    const parentLastName = 'Tester';
-    const parentNinoEligible = "nn123456c";
-    const parentNinoNotEligible = 'PN123456C'; // Updated to the specified NI number
-    const parentNinoParentNotFound = 'RA123456C'; // NI number for parent not found scenario
-    const parentLastNameNotFound = 'ttySimpson'; // Last name for parent not found scenario
-    const parentNinoTechnicalError = 'AA668767B'; // Using an unusual NI number to potentially trigger errors
 
-    it('Parent first and last names on Enter_Details should accept approved accented characters', () => {
+    it('Parent Last name on Enter_Details should accept approved accented characters', () => {
         //Setup - Get to Enter_Details page to perform test
         // Login with LA session
         cy.checkSession('LA');
@@ -18,29 +12,35 @@ describe('Test that approved accented characters are accepted in name input fiel
         cy.get('h1').should('include.text', 'Run a check for one parent or guardian');
         cy.contains('button', 'Early learning for 2-year-olds').click();
         cy.url().should('include', '/Check/Enter_Details');
-        //First and Last name fields should accept
-        // [("OBrien", "plain letters")]
-        // [("O'Brien", "straight apostrophe (U+0027)")]
-        // [("O\u2019Brien", "right curly apostrophe (U+2019)")]
-        // [("O\u2018Brien", "left curly apostrophe (U+2018)")]
-        // [("Smith-Jones", "hyphen")]
-        // [("St. Claire", "period and space")]
-        // [("van den Berg", "spaces")]
-        // [("ÁáÉéÍíÓóÚúÝýĆćĹĺŃńŔŕŚśŹź", "acute")]
-        // [("ÀàÈèÌìÒòÙùẀẁỲỳ", "grave")]
-        // [("ÂâÊêÎîÔôÛûĈĉĜĝĤĥĴĵŜŝŴŵŶŷ", "circumflex")]
-        // [("ÃãÑñÕõĨĩŨũẼẽỸỹ", "tilde")]
-        // [("ÄäËëÏïÖöÜüŸÿ", "umlaut or diaeresis")]
-        // [("ÇçĢģĶķĻļŅņŖŗŞşŢţ", "cedilla")]
-        // [("ÅåŮů", "ring")]
-        // [("ĀāĒēĪīŌōŪūȲȳ", "macron")]
-        // [("ĂăĔĕĞğĬĭŎŏŬŭ", "breve")]
-        // [("ĊċĖėĠġİẊẋŻż", "dot above")]
-        // [("ĄąĘęĮįŲų", "ogonek")]
-        // [("ŐőŰű", "double acute")]
-        cy.get('#LastName').type('OBrienO\'BrienO\u2019BrienO\u2018BrienSmith-JonesSt. Clairevan den BergÁáÉéÍíÓóÚúÝýĆćĹĺŃńŔŕŚśŹźÀàÈèÌìÒòÙùẀẁỲỳÀàÈèÌìÒòÙùẀẁỲỳÃãÑñÕõĨĩŨũẼẽỸỹÄäËëÏïÖöÜüŸÿÇçĢģĶķĻļŅņŖŗŞşŢţÅåŮůĀāĒēĪīŌōŪūȲȳĂăĔĕĞğĬĭŎŏŬŭĊċĖėĠġİẊẋŻżĄąĘęĮįŲųŐőŰű');
+
+        let approvedChars = "OBrien" + //plain letters
+            "O'Brien" + //straight apostrophe (U+0027)
+            "O\u2019Brien" + //right curly apostrophe (U+2019)
+            "O\u2018Brien" + //left curly apostrophe (U+2018)
+            "Smith-Jones" + //hyphen
+            "St. Claire" + //period and space
+            "van den Berg" + //spaces
+            "ÁáÉéÍíÓóÚúÝýĆćĹĺŃńŔŕŚśŹź" + //acute
+            "ÀàÈèÌìÒòÙùẀẁỲỳ" + //grave
+            "ÂâÊêÎîÔôÛûĈĉĜĝĤĥĴĵŜŝŴŵŶŷ" + //circumflex
+            "ÃãÑñÕõĨĩŨũẼẽỸỹ" + //tilde
+            "ÄäËëÏïÖöÜüŸÿ" + //umlaut or diaeresis
+            "ÇçĢģĶķĻļŅņŖŗŞşŢţ" + //cedilla
+            "ÅåŮů" + //ring
+            "ĀāĒēĪīŌōŪūȲȳ" + //macron
+            "ĂăĔĕĞğĬĭŎŏŬŭ" + //breve
+            "ĊċĖėĠġİẊẋŻż" + //dot above
+            "ĄąĘęĮįŲų" + //ogonek
+            "ŐőŰű"; //double acute
+
+        // Test the validation for Last name accepts the DWP predefined list of approved characters 
+        cy.get('#LastName').type(approvedChars);
         cy.contains('button', 'Run check').click();
         cy.get('#error-summary')
-            .should('not.contain.text', 'Enter a last name with valid characters');
+            .should('not.contain.text', "Enter a last name with valid characters");
+        //Verify that we did successfully submit the form because we received validation errors for the unfilled inputs
+        cy.get('#error-summary')
+            .should('contain.text', "Enter parent or guardian's date of birth")
+            .and('contain.text', "Enter a National Insurance number");
     });
 });
