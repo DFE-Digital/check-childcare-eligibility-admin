ab9eec28ad0f58997069e0351306d8d5fbb043e2
Harrison SHAW <Harrison.SHAW@EDUCATION.GOV.UK>
2026-08-07 15:14:20 +0100

    removal of commented out tile

--- CheckChildcareEligibility.Admin/Gateways/Interfaces/IMenuProvider.cs
+++ CheckChildcareEligibility.Admin/Gateways/Interfaces/IMenuProvider.cs
@@ -57,13 +57,12 @@ public class MenuProvider : IMenuProvider
                         "Home",
                         "MenuBulkCheck"
                         ),
-              /*      new MenuItem(
+                    new MenuItem(
                         "Run reports",
                         "Run reports",
                         "Run and export reports on all applications for childcare.",
                         "Report",
                         "Reports"),
-              */
                     new MenuItem(
                         "Guidance",
                         "Guidance",
